8e90997f0db01df2086415b0a2fcb7c0404ec50a
AmbroiseCollon <ambroise.collon@gmail.com>
2017-07-05 13:31:13 +0200

    Corrected UIImage => UIImageView

--- Discovering UIKit Components/TableViewController.swift
+++ Discovering UIKit Components/TableViewController.swift
@@ -8,88 +8,124 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+class TableViewController: UITableViewController, UITextFieldDelegate {
+    let sectionTitles = ["Générique", "Présentation", "Interaction Basique", "Sélecteurs", "Chargement"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        populateScrollView()
+        animateProgressView()
+        updateDateLabel(with: Date())
+        hideKeyboardWhenTappedAround()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: - UIScrollView
+    @IBOutlet weak var scrollView: UIScrollView!
+    func populateScrollView() {
+        scrollView.contentSize = CGSize(width: 50*10, height: 50*10)
+
+        for i in 0..<10 {
+            for j in 0..<10 {
+                if (i % 2 == 0 && j % 2 == 1) || (i % 2 == 1 && j % 2 == 0) {
+                    let view = UIView(frame: CGRect(
+                        origin: CGPoint(x: 50*i, y:50*j),
+                        size: CGSize(width: 50, height: 50)))
+                    view.backgroundColor = .customLightBlue
+                    scrollView.addSubview(view)
+                }
+            }
+        }
     }
 
-    // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    //MARK: - UIButton
+    @IBOutlet weak var buttonDescriptionLabel: UILabel!
+    @IBAction func tapButton() {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.buttonDescriptionLabel.alpha = 0
+        }) { (completed) in
+            if completed {
+                UIView.animate(withDuration: 0.2, animations: { 
+                    self.buttonDescriptionLabel.alpha = 1
+                })
+            }
+        }
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    //MARK: - UITextField
+    @IBOutlet weak var textfieldDescriptionLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBAction func editingTextField(_ sender: UITextField) {
+        textfieldDescriptionLabel.text = "text = \"\(sender.text!)\"\nplaceholder = \"Ecrivez ce que vous voulez\""
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    //MARK: - UISwitch
+    @IBOutlet weak var switchDescriptionLabel: UILabel!
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        let text = sender.isOn ? "true" : "false"
+        switchDescriptionLabel.text = "action = Value Changed\nisOn = " + text
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    //MARK: - UISegmentedControl
+    @IBOutlet weak var segmentedControlDescriptionLabel: UILabel!
+    @IBAction func segmentedControlChanged(_ sender: UISegmentedControl) {
+        segmentedControlDescriptionLabel.text = "action = Value Changed\nselectedSegmentIndex = \(sender.selectedSegmentIndex)"
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    //MARK: - UISlider
+    @IBOutlet weak var sliderDescriptionLabel: UILabel!
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        sliderDescriptionLabel.text = String(format: "action = Value Changed\nvalue = %.1f", sender.value)
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    //MARK: - UIStepper
+    @IBOutlet weak var stepperDescriptionLabel: UILabel!
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+        stepperDescriptionLabel.text = "stepValue = 1 / minimumValue = 0 / maximumValue = 10 / value = \(sender.value)"
     }
-    */
 
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    //MARK: - UIDatePickerView
+    @IBOutlet weak var datePickerDescriptionLabel: UILabel!
+    @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+        updateDateLabel(with: sender.date)
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func updateDateLabel(with date: Date) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+        let string = dateFormatter.string(from: date)
+        datePickerDescriptionLabel.text = "action = Value Changed\ndate = " + string
     }
-    */
 
+    //MARK: - UIProgressView
+    @IBOutlet weak var progressViewDescriptionLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    func animateProgressView() {
+        Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { _ in
+            var progress = self.progressView.progress
+            if progress < 1 {
+                progress += 0.01
+            } else {
+                progress = 0
+            }
+            self.progressView.progress = progress
+            self.progressViewDescriptionLabel.text = String(format: "progress = %.0f%%", progress * 100)
+        }
+    }
+
+    //MARK: - TableView
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView(frame: CGRect(
+            origin: CGPoint.zero,
+            size: CGSize(width: tableView.frame.width, height: tableView.sectionHeaderHeight)))
+        view.backgroundColor = UIColor.customDarkBlue
+
+        let label = UILabel(frame: view.frame)
+        label.text = sectionTitles[section]
+        label.font = UIFont.boldSystemFont(ofSize: 25)
+        label.textColor = .white
+        label.textAlignment = .center
+
+        view.addSubview(label)
+        return view
+    }
 }
