da6f422d09e2b10ff2918a4840db815316f24e45
Julien FERAND <hello@julienferand.dev>
2022-09-04 12:22:17 +0200

    Conversion to Swift5 + Safe Area in Coordinates View

--- Discovering UIKit Components/Helpers.swift
+++ Discovering UIKit Components/Helpers.swift
@@ -27,7 +27,7 @@ extension UIViewController {
         view.addGestureRecognizer(tap)
     }
 
-    func dismissKeyboard() {
+    @objc func dismissKeyboard() {
         view.endEditing(true)
     }
 }
